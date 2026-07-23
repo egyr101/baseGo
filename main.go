b43dbd04c8e8192f2282b1251855704f34886858
egkogladyshev <egkogladyshev@edu.hse.ru>
2026-07-23 18:59:09 +0500

    Написал метод вычисления рейтинга

--- main.go
+++ main.go
@@ -18,12 +18,16 @@ func NewPlayer(name string, goals, misses, assists int) Player {
 		Goals:   goals,
 		Misses:  misses,
 		Assists: assists,
-		Rating:  calculateRating(),
+		Rating:  calculateRating(float64(goals), float64(misses), float64(assists)),
 	}
 }
 
-func calculateRating() float64 {
-	return 0
+func calculateRating(goals, misses, assists float64) float64 {
+	if misses == 0 {
+		return (goals + assists) / 2
+	}
+
+	return (goals + assists) / 2 / misses
 }
 
 func goalsSort(players []Player) []Player {
