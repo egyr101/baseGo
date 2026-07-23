6e4ccc55e3e6de3912e8c533c2fbda69a187f451
egkogladyshev <egkogladyshev@edu.hse.ru>
2026-07-23 18:56:29 +0500

    Добавил заглушки нужных методов и конструктор структуры

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+type Player struct {
+	Name    string
+	Goals   int
+	Misses  int
+	Assists int
+	Rating  float64
+}
+
 func main() {
 
+}
+
+func NewPlayer(name string, goals, misses, assists int) Player {
+	return Player{
+		Name:    name,
+		Goals:   goals,
+		Misses:  misses,
+		Assists: assists,
+		Rating:  calculateRating(),
+	}
+}
+
+func calculateRating() float64 {
+	return 0
+}
+
+func goalsSort(players []Player) []Player {
+	return nil
+}
+
+func ratingSort(players []Player) []Player {
+	return nil
+}
+
+func gmSort(players []Player) []Player {
+	return nil
 }
